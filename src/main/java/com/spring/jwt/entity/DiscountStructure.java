58c3044eef81856688910fc919bfdef07759db3d
Ashutosh7796 <ashutoshshedgeas87@gmail.com>
2025-10-01 23:36:03 +0530

    ADDED: Discount and All

--- src/main/java/com/spring/jwt/entity/DiscountStructure.java
+++ src/main/java/com/spring/jwt/entity/DiscountStructure.java
@@ -1,27 +1,31 @@
 package com.spring.jwt.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
-public class DiscountStructure
-{
+public class DiscountStructure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer DiscountId;
+    @Column(name = "discount_id")
+    private Integer discountId;
 
+    @Column(name = "manufacturer")
     private String manufacturer;
+
+    @Column(name = "discount_a")
     private Integer discountA;
+
+    @Column(name = "discount_b")
     private Integer discountB;
+
+    @Column(name = "discount_c")
     private Integer discountC;
 
+    @Column(name = "active_set_index")
     private Integer activeSetIndex;
-
-
 }
+
